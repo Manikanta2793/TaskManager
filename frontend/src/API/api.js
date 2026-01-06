0ab0dd36cb30be61d5e2d425a8784b865e1d3780
Manikanta2793 <chethelliravinder@gmail.com>
2026-01-06 14:38:55 +0530

    updated api.js

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'http://localhost:5000/api',
+    baseURL: 'https://taskmanager-6iwr.onrender.com/api',
 });
 
+// Attach stored JWT to every request so protected endpoints succeed
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 export default API;
